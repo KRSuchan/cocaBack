aad83fe7d09d23b1891fa6edc955c9b7ec3ee33e
KRSuchan <lsc1814@naver.com>
2024-05-05 06:39:41 +0900

    임시 커밋

---
+++ src/main/java/project/coca/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package project.coca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.coca.domain.personal.Member;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+}
