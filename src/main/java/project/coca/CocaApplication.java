ad32c8402b6e7ec4615b5fb2c5a1fcdeebf96ba4
ikellllllll <shkm1420@naver.com>
2024-05-07 17:08:09 +0900

    [Main :: CORS 권한 수정] * localhost:3000에 대해서만 허용

--- src/main/java/project/coca/CocaApplication.java
+++ src/main/java/project/coca/CocaApplication.java
@@ -3,6 +3,9 @@ package project.coca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CocaApplication {
@@ -11,4 +14,13 @@ public class CocaApplication {
 		SpringApplication.run(CocaApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
