ef9137f1cc88b39ef921cf4e933e9b1ebb28154c
KRSuchan <lsc1814@naver.com>
2024-05-10 21:03:57 +0900

    그룹 일정의 가져오기 기능을 위한 도메인 수정 및 추가(GroupScheduleHeart.java)

--- src/main/java/project/coca/domain/group/GroupScheduleHeart.java
+++ src/main/java/project/coca/domain/group/GroupScheduleHeart.java
@@ -17,7 +17,7 @@ public class GroupScheduleHeart {
     @Column(name = "GROUP_SCHEDULE_HEART_ID")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "GROUP_MEMBER_ID", nullable = false)
     private GroupMember groupMember;
 
