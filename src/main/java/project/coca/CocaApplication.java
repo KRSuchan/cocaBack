d0f27efe0cad366667d38dca405a7ca68cd58461
ikellllllll <shkm1420@naver.com>
2024-05-13 20:02:52 +0900

    [Main :: CORS 에러 수정]

--- src/main/java/project/coca/CocaApplication.java
+++ src/main/java/project/coca/CocaApplication.java
@@ -19,7 +19,9 @@ public class CocaApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
 			}
 		};
 	}
