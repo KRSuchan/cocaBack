356274a16f110da75490f6aea8ea007f54bec68a
dkki4887 <4887chaeg@gmail.com>
2024-05-21 22:54:15 +0900

    groupScheduleRegistrationRequest

--- src/main/java/project/coca/dto/request/GroupScheduleRequest.java
+++ src/main/java/project/coca/dto/request/GroupScheduleRequest.java
@@ -1,15 +1,26 @@
 package project.coca.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import project.coca.domain.group.GroupSchedule;
-import project.coca.domain.group.GroupScheduleAttachment;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class GroupScheduleRequest {
-    private GroupSchedule groupSchedule;
-    private List<GroupScheduleAttachment> attachments;
+    private String memberId; //수정을 시도한 멤버 아이디
+    private Long groupId;
+    private String title;
+    private String description;
+    private String location;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endTime;
+    private String color;
+    private List<GroupScheduleAttachmentRequest> attachments;
 }
