df6f368526f8a26daa28661e5892a555710be956
dkki4887 <4887chaeg@gmail.com>
2024-05-23 15:59:25 +0900

    groupScheduleRegistration fix2

---
+++ src/main/java/project/coca/dto/response/group/GroupScheduleResponse.java
@@ -0,0 +1,47 @@
+package project.coca.dto.response.group;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import project.coca.domain.group.GroupSchedule;
+import project.coca.domain.group.GroupScheduleAttachment;
+import project.coca.domain.group.GroupScheduleHeart;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
+@Getter
+@Setter
+public class GroupScheduleResponse {
+    private Long groupId;
+    private String title;
+    private String description;
+    private String location;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endTime;
+    private String color;
+    private int hearts; //하트 개수
+    private List<GroupScheduleAttachmentResponse> attachments;
+
+    public static GroupScheduleResponse of(GroupSchedule schedule) {
+        return GroupScheduleResponse.builder()
+                .groupId(schedule.getCoGroup().getId())
+                .title(schedule.getTitle())
+                .description(schedule.getDescription())
+                .location(schedule.getLocation())
+                .startTime(schedule.getStartTime())
+                .endTime(schedule.getEndTime())
+                .color(schedule.getColor())
+                .hearts(schedule.getHearts().size())
+                .attachments(schedule.getGroupScheduleAttachments()
+                        .stream().map(GroupScheduleAttachmentResponse::of)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+}
