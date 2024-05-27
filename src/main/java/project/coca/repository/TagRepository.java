4b14541351eb31980e9124c23f7d49855fc998c0
KRSuchan <lsc1814@naver.com>
2024-05-27 17:16:21 +0900

    존재하지 않는 태그로 조회시 null 리턴하도록 수정

--- src/main/java/project/coca/repository/TagRepository.java
+++ src/main/java/project/coca/repository/TagRepository.java
@@ -3,6 +3,8 @@ package project.coca.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.coca.domain.tag.Tag;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    Tag findByName(String name);
+    Optional<Tag> findByName(String name);
 }
