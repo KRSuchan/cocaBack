afca3da67661602affc4a10bcb472743227ac9b5
KRSuchan <lsc1814@naver.com>
2024-06-02 16:44:37 +0900

    그룹 일정 보강

--- src/main/java/project/coca/controller/GroupScheduleController.java
+++ src/main/java/project/coca/controller/GroupScheduleController.java
@@ -111,8 +111,10 @@ public class GroupScheduleController {
 
             return ApiResponse.response(ResponseCode.OK, updateGroupSchedule);
         } catch (NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
             return ApiResponse.fail(ErrorCode.NOT_FOUND, "조회되지 않는 데이터가 포함되어 있습니다.");
         } catch (Exception e) {
+            System.out.println(e.getMessage());
             return ApiResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
