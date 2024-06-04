a2906949af468991561de4007ef0d2df7fb188ee
= <=>
2024-06-05 06:36:42 +0900

    get to post mapping findemptyschedulereq

--- src/main/java/project/coca/controller/CommonScheduleController.java
+++ src/main/java/project/coca/controller/CommonScheduleController.java
@@ -1,7 +1,7 @@
 package project.coca.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,13 +22,13 @@ public class CommonScheduleController {
 
     private final CommonScheduleService commonScheduleService;
 
-    @GetMapping("/findEmptyScheduleReq")
+    @PostMapping("/findEmptyScheduleReq")
     public ApiResponse<List<EmptySchedule>> findEmptyScheduleReq(@RequestBody FindEmptyScheduleRequest request) {
         try {
             List<EmptySchedule> result = commonScheduleService.findEmptySchedule(request);
 
             return ApiResponse.response(ResponseCode.OK, result);
-        } catch(NoSuchElementException e) {
+        } catch (NoSuchElementException e) {
             return ApiResponse.fail(ErrorCode.NOT_FOUND, "찾을 일정의 시간/날을 입력하지 않았습니다.");
         } catch (Exception e) {
             return ApiResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage());
