880929e31c8b7ef8302f43ab47ac77a4da806c79
= <=>
2024-06-05 11:01:25 +0900

    일정 첨부파일 및 캘린더 isManager 추가

--- src/main/java/project/coca/dto/response/group/CalendarResponse.java
+++ src/main/java/project/coca/dto/response/group/CalendarResponse.java
@@ -11,12 +11,14 @@ public class CalendarResponse {
     private Long groupId;
     private String groupName;
     private Boolean isAdmin;
+    private Boolean isManager;
 
     public static CalendarResponse of(CoGroup group, Member member) {
         return CalendarResponse.builder()
                 .groupId(group.getId())
                 .groupName(group.getName())
                 .isAdmin(group.getAdmin().getId().equals(member.getId()))
+                .isManager(group.getGroupManager().stream().anyMatch(groupManager -> groupManager.getId().equals(group.getId())))
                 .build();
     }
 }
