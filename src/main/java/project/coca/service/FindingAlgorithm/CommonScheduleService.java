df65659b30cf3eb1d915677bc30fcae30dc552dd
= <=>
2024-06-07 09:50:04 +0900

    마지막 커밋

--- src/main/java/project/coca/service/FindingAlgorithm/CommonScheduleService.java
+++ src/main/java/project/coca/service/FindingAlgorithm/CommonScheduleService.java
@@ -11,7 +11,6 @@ import project.coca.dto.response.CommonSchedule;
 import project.coca.dto.response.personalSchedule.PersonalScheduleForEmptyScheduleResponse;
 import project.coca.repository.MemberRepository;
 import project.coca.repository.PersonalScheduleRepository;
-import project.coca.service.FindingAlgorithm.Interval;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -40,11 +39,11 @@ public class CommonScheduleService {
 
         List<PersonalScheduleForEmptyScheduleResponse> result = new ArrayList<>();
 
-        if(memberList.getMembers() != null && memberList.getMembers().size() > 0) {
-            for(String memberId : memberList.getMembers()) {
+        if (memberList.getMembers() != null && memberList.getMembers().size() > 0) {
+            for (String memberId : memberList.getMembers()) {
                 Member member = memberRepository.findById(memberId).orElseThrow(() -> new NoSuchElementException("회원이 조회되지 않습니다."));
                 List<CommonSchedule> schedule = personalScheduleRepository.findPersonalScheduleByDateRange(memberId, startTime, endTime)
-                                .stream().map(CommonSchedule::of).collect(Collectors.toList());
+                        .stream().map(CommonSchedule::of).collect(Collectors.toList());
 
                 result.add(new PersonalScheduleForEmptyScheduleResponse(member.getId(), member.getUserName(), schedule));
             }
@@ -53,18 +52,16 @@ public class CommonScheduleService {
     }
 
     public List<CommonSchedule> findEmptySchedule(FindEmptyScheduleRequest request) {
-        if(request.getFindDay() > 0) {
+        if (request.getFindDay() > 0) {
             int period = (int) ChronoUnit.DAYS.between(request.getStartDate(), request.getEndDate()) + 1; //if(24~29)면 6일 필요
 
             return bruteForce(request.getStartDate(), request.getFindDay(), period, request.getMembers());
-        }
-        else if(request.getFindMinute() > 0) {
+        } else if (request.getFindMinute() > 0) {
             LocalDateTime startTime = request.getStartDate().atStartOfDay();
             LocalDateTime endTime = request.getEndDate().atTime(23, 59, 59);
 
             return interval(startTime, endTime, request.getFindMinute(), request.getMembers());
-        }
-        else {
+        } else {
             throw new NoSuchElementException("찾을 일정의 시간/날을 입력하지 않았습니다.");
         }
     }
@@ -73,16 +70,16 @@ public class CommonScheduleService {
         boolean daySlot[] = new boolean[period];
         Arrays.fill(daySlot, true);
 
-        for(String memberId : members) {
+        for (String memberId : members) {
             LocalDateTime startTime = startDate.atTime(0, 0, 1);
             LocalDateTime endTime = startDate.atTime(23, 59, 59);
 
-            for(int i = 0; i < period; i++) {
+            for (int i = 0; i < period; i++) {
                 //이미 그날에 일정이 있는거면(daySlot[i] == false) 굳이 확인할 필요 없음. 이미 땡인 날임
-                if(daySlot[i]) {
+                if (daySlot[i]) {
                     List<PersonalSchedule> personalSchedules = personalScheduleRepository.findPersonalScheduleByDateRange(memberId, startTime, endTime);
 
-                    if(personalSchedules != null && personalSchedules.size() > 0)
+                    if (personalSchedules != null && personalSchedules.size() > 0)
                         daySlot[i] = false;
                 }
                 startTime = startTime.plusDays(1);
@@ -92,10 +89,10 @@ public class CommonScheduleService {
 
         List<CommonSchedule> resultSchedule = new ArrayList<>();
 
-        for(int i = 0; i <= daySlot.length - duration; i++) {
+        for (int i = 0; i <= daySlot.length - duration; i++) {
             boolean isAvailable = true;
-            for(int j = 0; j < duration; j++) {
-                if(!daySlot[i + j]) {
+            for (int j = 0; j < duration; j++) {
+                if (!daySlot[i + j]) {
                     isAvailable = false;
                     break;
                 }
@@ -110,11 +107,11 @@ public class CommonScheduleService {
         List<Interval> combined = new ArrayList<>();
         final int tineSlot = 10;
 
-        for(String memberId : members) {
+        for (String memberId : members) {
             List<PersonalSchedule> personalSchedules = personalScheduleRepository.findPersonalScheduleByDateRange(memberId, startTime, endTime);
 
-            if(personalSchedules != null && personalSchedules.size() > 0){
-                for(PersonalSchedule schedule : personalSchedules)
+            if (personalSchedules != null && personalSchedules.size() > 0) {
+                for (PersonalSchedule schedule : personalSchedules)
                     combined.add(new Interval(schedule.getStartTime(), schedule.getEndTime()));
             }
         }
@@ -124,8 +121,7 @@ public class CommonScheduleService {
         List<CommonSchedule> resultSchedule = new ArrayList<>(); //빈 일정이 담기는 리스트
         LocalDateTime current = startTime;
 
-        for (Interval interval : mergeSchedule)
-        {
+        for (Interval interval : mergeSchedule) {
             while ((current.plusMinutes(duration).isBefore(interval.getStart()) || current.plusMinutes(duration).isEqual(interval.getStart()))) {
                 resultSchedule.add(new CommonSchedule(current, current.plusMinutes(duration)));
                 current = current.plusMinutes(tineSlot); // 다음 빈 시간대를 검색하기 위해 시간 증가
