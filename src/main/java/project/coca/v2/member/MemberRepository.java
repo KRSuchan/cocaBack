4e03d05b3698d62b2390d097d8a4873a8fb5912d
KRSuchan <lsc1814@naver.com>
2024-12-13 14:10:50 +0900

    v1 임시 조치

--- src/main/java/project/coca/v2/member/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package project.coca.v2.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.coca.domain.personal.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
--- src/main/java/project/coca/v2/member/MemberServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package project.coca.v2.member;
-
-public class MemberServiceImpl implements MemberService {
-}
