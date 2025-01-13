5a769407fbfe3654868208e233f81d310e96e371
KRSuchan <lsc1814@naver.com>
2025-01-13 17:34:37 +0900

    HTTPS 작업 - class HttpController 추가 - healthcheck 메소드 추가

---
+++ src/main/java/project/coca/security/HttpController.java
@@ -0,0 +1,12 @@
+package project.coca.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HttpController {
+    @GetMapping("/healthcheck")
+    public String healthcheck() {
+        return "OK";
+    }
+}
